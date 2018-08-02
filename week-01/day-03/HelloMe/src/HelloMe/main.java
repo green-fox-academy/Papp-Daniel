b3847372148ef1aa8248d94c7c586a960c91f4ce
Papp Daniel <pappdaniel012@gmail.com>
2018-08-02 13:01:53 +0200

    01-03 done

---
+++ week-01/day-03/HelloMe/src/HelloMe/main.java
@@ -0,0 +1,8 @@
+package HelloMe;
+
+public class main {
+    public static void main (String[] args) {
+        //I've modified this program to greet ME
+        System.out.println("Hello, Daniel!");
+    }
+}
