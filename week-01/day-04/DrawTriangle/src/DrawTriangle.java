1357f306b6fb535c9a854f45aa7256e84080fc75
Papp Daniel <pappdaniel012@gmail.com>
2018-08-05 09:26:50 +0200

    modify DrawTriangle

--- week-01/day-04/DrawTriangle/src/DrawTriangle.java
+++ week-01/day-04/DrawTriangle/src/DrawTriangle.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class DrawTriangle {
     public static void main(String[] args) {
 
-        int size = 6;
+        System.out.println("Enter a number :");
+
+        Scanner UserIn = new Scanner(System.in);
+
+
+        int size = UserIn.nextInt();
 
         for (int i=1; i<=size; i++){
             for (int j=1; j<=size;j++){
