333e9859f53919b0dbd52fa609c32f2ec83e28fe
Papp Daniel <pappdaniel012@gmail.com>
2018-08-06 14:36:50 +0200

    Doubling finish

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,17 @@
+public class Doubling {
+
+    public static void main(String[] args) {
+
+        int baseNum = 123;
+        doubling();
+
+    }
+
+    public static void doubling() {
+        int baseNum = 123;
+        int result = baseNum*2;
+        System.out.println("doubling" +"("+result+")");
+
+    }
+
+}
