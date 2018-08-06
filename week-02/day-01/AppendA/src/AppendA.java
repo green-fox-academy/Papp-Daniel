fd2593b3db4420ecc12305c3075f004cde210137
Papp Daniel <pappdaniel012@gmail.com>
2018-08-06 15:26:13 +0200

    Append a finish

---
+++ week-02/day-01/AppendA/src/AppendA.java
@@ -0,0 +1,14 @@
+public class AppendA {
+
+    public String typo = "Chinchill";
+
+    public static void main(String[] args) {
+
+        appendAFunc("Chinchill");
+
+    }
+
+    public static void appendAFunc(String typo) {
+        System.out.println(typo +"a");
+    }
+}
