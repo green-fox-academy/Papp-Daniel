4bf59b421a1ae7e5797cb9b8e3ff4bb43bc779f6
Papp Daniel <pappdaniel012@gmail.com>
2018-08-06 17:07:47 +0200

    feat(SumElements): SumElements finish

---
+++ week-02/day-01/SumElements/src/SumElements.java
@@ -0,0 +1,10 @@
+public class SumElements {
+    public static void main(String[] args) {
+
+        int[] r = {54, 23, 66, 12};
+
+        int result = r[2] + r[3];
+
+        System.out.println("The sum of the second and third element : "+result);
+    }
+}
