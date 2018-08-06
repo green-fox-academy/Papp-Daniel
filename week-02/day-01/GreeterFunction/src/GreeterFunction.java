1ba679d7058e713c92f90021e8fa1d33e54a07b2
Papp Daniel <pappdaniel012@gmail.com>
2018-08-06 14:51:34 +0200

    GreeterFunciton finish

---
+++ week-02/day-01/GreeterFunction/src/GreeterFunction.java
@@ -0,0 +1,16 @@
+public class GreeterFunction {
+
+    static String al = "GreenFox";
+
+    public static void main(String[] args) {
+
+        greet();
+
+
+    }
+
+    public static void greet() {
+        System.out.println("Greetings dear ,"+al);
+
+    }
+}
