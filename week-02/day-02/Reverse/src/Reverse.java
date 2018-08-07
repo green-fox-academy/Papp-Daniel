fd07b8c02dca626d71789f7e8abe4374058b3034
Papp Daniel <pappdaniel012@gmail.com>
2018-08-07 14:11:08 +0200

    feat(Reverse): finish Reverse

---
+++ week-02/day-02/Reverse/src/Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+    public static void main(String[] args) {
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        String s2 = new StringBuffer(reversed).reverse().toString();
+
+        System.out.println("String after reverseing is: " + s2);
+
+    }
+
+}
