1a6bebb3460022175724e2e21c06d6ad95176ff5
Papp Daniel <pappdaniel012@gmail.com>
2018-08-08 17:12:34 +0200

    feat(Checkerboard) : is not finish

---
+++ week-02/day-03/Checkerboard/src/Checkerboard.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+    public static void mainDraw(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern.
+
+        int x = 10;
+        int y = 10;
+
+       line1(x,y,graphics);
+       
+
+    }
+
+    public static void line1 (int x, int y, Graphics graphics){
+        graphics.setColor(Color.black);
+        for (int i =0; i<20;i+=10){
+            for (int j =0; j<20; j+=10){
+            graphics.fillRect(i,j,x,y);
+            }
+        }
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
