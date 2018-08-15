c69946c6543b409cb77d56b3828d9871396857a8
Papp Daniel <pappdaniel012@gmail.com>
2018-08-15 16:22:10 +0200

    feat(Dominoes) : finish

---
+++ week-03/day-03/Dominoes/src/Domino.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Domino {
+int[] values;
+
+    Domino(int valueA, int valueB) {
+        this.values = new int[]{valueA, valueB};
+    }
+
+    int[] getValues() {
+        return values;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + values[0] + ", " + values[1] + "]";
+    }
+}
