1be86fec7dd3e0b00a4271b350cca252bf7ab194
Papp Daniel <pappdaniel012@gmail.com>
2018-08-16 11:36:23 +0200

    feat(Sharpie) : make a class named Sharpie and a constuctor in it

---
+++ week-03/day-04/Sharpie/src/Sharpie.java
@@ -0,0 +1,15 @@
+public class Sharpie {
+    String color;
+    float width;
+    float inkAmount;
+
+    Sharpie(String color, float width, float inkAmount) {
+        this.color = color;
+        this.width = width;
+        this.inkAmount = 100;
+    }
+
+    void use() {
+        this.inkAmount -= 10;
+    }
+}
