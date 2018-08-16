b55baeb65d6e3fa848ab9580e84976976100e729
Papp Daniel <pappdaniel012@gmail.com>
2018-08-16 11:39:39 +0200

    feat(Sharpie) : make an object in the Main and add the use() method

--- week-03/day-04/Sharpie/src/Main.java
+++ week-03/day-04/Sharpie/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Sharpie sharpie1 = new Sharpie("red", 33, 100);
+        sharpie1.use();
 
+        System.out.println(sharpie1.inkAmount);
     }
 }
