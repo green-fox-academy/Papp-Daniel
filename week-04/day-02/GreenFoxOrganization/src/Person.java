b2416431fca0601a1be086d1614007e4be2a79a5
Papp Daniel <pappdaniel012@gmail.com>
2018-08-21 13:35:12 +0200

    feat(GreenFoxOrganization) : add Main, Person and Student class

---
+++ week-04/day-02/GreenFoxOrganization/src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    String name;
+    int age;
+    String gender;
+
+    void indtroduce(String name, int age, String gender) {
+        System.out.println("Hi I am " + name + "a " + age + " years old " + gender + "!");
+    }
+
+    void getGoal(){
+        System.out.println("I wanne be the very best, like no one ever was!");
+    }
+    Person(String name, int age, String gender){
+
+    }
+    Person(){
+        this.name = "Jane Doe";
+        this.age = 30;
+        this.gender = "female";
+    }
+}
