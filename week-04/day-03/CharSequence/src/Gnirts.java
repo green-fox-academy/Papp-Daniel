27d7486a942df08d62e7481e7630b1467c5993c6
Papp Daniel <pappdaniel012@gmail.com>
2018-08-22 15:00:19 +0200

    feat(CharSequence) : add Main and Gnirts class

---
+++ week-04/day-03/CharSequence/src/Gnirts.java
@@ -0,0 +1,22 @@
+public class Gnirts implements CharSequence {
+    String text;
+
+    Gnirts(String text){
+        this.text = text;
+    }
+
+    @Override
+    public int length() {
+        return text.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return text.charAt(index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return text.subSequence(start,end);
+    }
+}
---
+++ week-04/day-03/CharSequence/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    Gnirts gnirts = new Gnirts("asdfsd");
+
+        System.out.println(gnirts.text);
+        System.out.println(gnirts.text.length());
+        System.out.println(gnirts.text.charAt(2));
+        System.out.println(gnirts.text.subSequence(1,5));
+
+    }
+}
