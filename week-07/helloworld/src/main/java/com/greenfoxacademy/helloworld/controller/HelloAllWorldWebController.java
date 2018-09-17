0aefe75c641f7878ebbd965d69908192b2c04c4b
Papp Daniel <pappdaniel012@gmail.com>
2018-09-17 17:32:46 +0200

    update WebApp

---
+++ week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controller/HelloAllWorldWebController.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HelloAllWorldWebController {
+
+
+    @GetMapping("hellos")
+    public String printHellos(Model model) {
+        String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+        model.addAttribute("hellos",hellos);
+
+        return "hellos";
+    }
+}
--- week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controller/HelloWebController.java
+++ week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controller/HelloWebController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController 
+@RestController
 public class HelloWebController {
     AtomicLong id = new AtomicLong();
 
