ae3565968ec655d00204054c68906c4f0432f0f2
daniboy01 <pappdaniel012@gmail.com>
2018-09-25 17:23:04 +0200

    finish 3rd task

--- week-08/todoProject/src/main/java/com/greenfox/todo/controllers/TodoController.java
+++ week-08/todoProject/src/main/java/com/greenfox/todo/controllers/TodoController.java
@@ -1,11 +1,17 @@
 package com.greenfox.todo.controllers;
 
+import com.greenfox.todo.models.Todo;
 import com.greenfox.todo.repository.TodoRepository;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -18,16 +24,37 @@ public class TodoController {
     }
 
 
-    @RequestMapping("/todo")
-    public String list(@RequestParam("isActive")boolean isActive, Model model) {
-        model.addAttribute("todoList", todoRepository.findAll());
+    @RequestMapping(value = {"/todo", "/"})
+    public String list(@RequestParam(required = false) Boolean isActive, Model model) {
+        if (isActive == null) {
+            model.addAttribute("todoList", todoRepository.findAll());
+        } else if (isActive==true) {
+            List<Todo> isNotDone = Streamable.of(todoRepository.findAll()).stream()
+                    .filter(a -> !a.isDone())
+                    .collect(Collectors.toList());
+            model.addAttribute("todoList", isNotDone);
+        } else if (isActive==false) {
+            List<Todo> isNotDone = Streamable.of(todoRepository.findAll()).stream()
+                    .filter(a -> a.isDone())
+                    .collect(Collectors.toList());
+            model.addAttribute("todoList", isNotDone);
 
+        }
         return "todo";
+    }
+    @GetMapping("/addTodo")
+    public String getaddTodo() {
+        return "addTodo";
     }
 
     @PostMapping("/addTodo")
-    public String addTodo(Model model, String todo) {
-        return "addTodo";
+    public String addNewTodo(String title) {
+        Todo todo = new Todo();
+        todo.setTitle(title);
+        todoRepository.save(todo);
 
+        return "redirect:/";
     }
+
 }
+
