f4ab523b45cc85d5515b353835ff35cd754047f2
daniboy01 <pappdaniel012@gmail.com>
2018-09-27 16:50:28 +0200

    modify search option method to GET

--- week-08/todoProject/src/main/java/com/greenfox/todo/controllers/SearchController.java
+++ week-08/todoProject/src/main/java/com/greenfox/todo/controllers/SearchController.java
@@ -1,13 +1,10 @@
 package com.greenfox.todo.controllers;
 
-import com.greenfox.todo.models.Todo;
+
 import com.greenfox.todo.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -18,7 +15,7 @@ public class SearchController {
         this.todoRepository = todoRepository;
     }
 
-    @PostMapping("/search")
+    @GetMapping("/search")
     public String searchTodo(String title, Model model) {
         model.addAttribute("todoList",todoRepository.findByTitle(title));
         return "todo";
